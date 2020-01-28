b3c739eb7f1aee6c0a2c7d368ce5d5a8420256c4
david <albaktiar10@gmail.com>
2020-01-29 00:07:42 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import QRScanner from './QRScanner';
+
+function App() {
+  return (
+    <div className='App'>
+      <QRScanner />
+    </div>
+  );
+}
+
+export default App;
